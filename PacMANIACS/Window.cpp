b4155411c1e558c00ad50599c9d8986749aab9c4
Johan <johan_almeflo@hotmail.com>
2013-02-20 02:17:46 +0100

    asd

--- PacMANIACS/Window.cpp
+++ PacMANIACS/Window.cpp
@@ -29,10 +29,10 @@ ID3D11DeviceContext*	g_DeviceContext			= NULL;
 
 Camera*					camera					= new Camera(D3DXVECTOR3(0, 0, -150), 
 															D3DXVECTOR3(0, 0, 1), 5);
-Model*					model					= new Model();
+/*Model*					model					= new Model();
 Model*					plane					= new Model();
 
-Model*					lampSphere				= new Model();
+Model*					lampSphere				= new Model();*/
 D3DXVECTOR3				lampPosition			= D3DXVECTOR3(0, 0, -50);
 
 
