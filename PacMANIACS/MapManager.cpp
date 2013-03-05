f44dcd9550b61b6b3777183c737f0fb30aa547a7
CarlRapp <Carl.Sebastian.Rapp@gmail.com>
2013-03-05 15:20:50 +0100

    Fixed MapManager to work with GameObjectManager

--- PacMANIACS/MapManager.cpp
+++ PacMANIACS/MapManager.cpp
@@ -102,3 +102,13 @@ map<char, string> MapManager::GetGameObjectList()
 {
 	return m_gameObjectList;
 }
+
+
+int MapManager::GetWidth()
+{
+	return m_mapWidth;
+}
+int MapManager::GetHeight()
+{
+	return m_mapHeight;
+}
