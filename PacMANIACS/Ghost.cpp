d57767e5260cb3e2a35e9557d26961de8c1f0b8d
TENFIRE <nil_erik@hotmail.com>
2013-03-07 20:38:01 +0100

    Added CalculateMove for Ghost. Moved GhostAIState to Ghost (temporary?)

--- PacMANIACS/Ghost.cpp
+++ PacMANIACS/Ghost.cpp
@@ -1,6 +1,56 @@
 #include "Ghost.h"
 
 
+string StupidGhostAIState::GetTextureName()
+{
+	return "Ghost_Orange_Texture.png";
+}
+
+void StupidGhostAIState::CalculateMove(GameObject* ghost, vector<D3DXVECTOR3> availableMoves)
+{
+	if (availableMoves.size() >= 4)
+		ghost->MoveTo(availableMoves[0]);
+	else
+		ghost->MoveTo(availableMoves[availableMoves.size() - 1]);
+}
+
+string NormalGhostAIState::GetTextureName()
+{
+	return "Ghost_Blue_Texture.png";
+}
+
+void NormalGhostAIState::CalculateMove(GameObject* ghost, vector<D3DXVECTOR3> availableMoves)
+{
+	if (availableMoves.size() >= 3)
+		ghost->MoveTo(availableMoves[0]);
+	else
+		ghost->MoveTo(availableMoves[availableMoves.size() - 1]);
+}
+
+string SmartGhostAIState::GetTextureName()
+{
+	return "Ghost_Purpel_Texture.png";
+}
+
+void SmartGhostAIState::CalculateMove(GameObject* ghost, vector<D3DXVECTOR3> availableMoves)
+{
+	if (availableMoves.size() >= 2)
+		ghost->MoveTo(availableMoves[0]);
+	else
+		ghost->MoveTo(availableMoves[availableMoves.size() - 1]);
+}
+
+string GeniusGhostAIState::GetTextureName()
+{
+	return "Ghost_Red_Texture.png";
+}
+
+void GeniusGhostAIState::CalculateMove(GameObject* ghost, vector<D3DXVECTOR3> availableMoves)
+{
+	if (availableMoves.size() >= 1)
+		ghost->MoveTo(availableMoves[0]);
+}
+
 Ghost::Ghost(void) : GameObject()
 {
 	gAIState = new NormalGhostAIState();
@@ -29,4 +79,9 @@ float Ghost::GetHitRadius()
 void Ghost::SetAIState(GhostAIState* AIState)
 {
 	gAIState = AIState;
+}
+
+void Ghost::CalculateMove(GameObject* target)
+{
+
 }
--- PacMANIACS/GhostAIState.h
+++ PacMANIACS/GhostAIState.h
@@ -1,7 +1,7 @@
 #pragma once
 #ifndef GHOSTAISTATE_H
 #define GHOSTAISTATE_H	
-
+/*
 #include <string>
 
 using namespace std;
@@ -10,30 +10,36 @@ class GhostAIState
 {
 public:
 	virtual string GetTextureName() = 0;
+	//virtual void CalculateMove(GameObject* target) = 0;
 };
 
 class StupidGhostAIState : public GhostAIState
 {
 public:
 	string GetTextureName();
+	//void CalculateMove(GameObject* target);
 };
 
 class NormalGhostAIState : public GhostAIState
 {
 public:
 	string GetTextureName();
+	//void CalculateMove(GameObject* target);
 };
 
 class SmartGhostAIState : public GhostAIState
 {
 public:
 	string GetTextureName();
+	//void CalculateMove(GameObject* target);
 };
 
 class GeniusGhostAIState : public GhostAIState
 {
 public:
 	string GetTextureName();
+	//void CalculateMove(GameObject* target);
 };
 
+*/
 #endif
